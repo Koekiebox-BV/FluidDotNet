b455e7785b3712ce3746d1e7f994137b683356ae
koekiebox <jason@koekiebox.com>
2016-12-30 18:52:25 +0400

    Initial commit for FluidDotNet.

---
+++ FluidAPI/Com/Fluid/Vo/ABaseFluidJSONObject.cs
@@ -0,0 +1,81 @@
+﻿#region License
+/*
+ * Koekiebox CONFIDENTIAL
+ *
+ * [2012] - [2017] Koekiebox (Pty) Ltd
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property
+ * of Koekiebox and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to Koekiebox
+ * and its suppliers and may be covered by South African and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Koekiebox.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Base Fluid class for all value objects.
+/// </summary>
+/// <remarks>
+/// 
+/// </remarks>
+namespace FluidAPI.Com.Fluid.Vo
+{
+    /// <summary>
+    /// Base JSON value object for Fluid related objects.
+    /// </summary>
+    public abstract class ABaseFluidJSONObject
+    {
+        
+
+        /// <summary>
+        /// Get and set the id.
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public long Id { get; set; }
+
+
+        /// <summary>
+        /// Get and set the service ticket.
+        /// </summary>
+        [JsonProperty(PropertyName = "serviceTicket")]
+        public string ServiceTicket { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="baseFluidJsonObjParam"></param>
+        /// <returns></returns>
+        public static string ObjectToString(ABaseFluidJSONObject baseFluidJsonObjParam)
+        {
+            return JsonConvert.SerializeObject(baseFluidJsonObjParam);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jsonStringParam">The JSON text to parse.</param>
+        /// <returns></returns>
+        public static object StringToObject(string jsonStringParam)
+        {
+            return JsonConvert.DeserializeObject(jsonStringParam);
+        }
+
+
+
+
+    }
+}
---
+++ FluidWSDotNetClient/Com/Fluid/Ws/Client/V1/ABaseClientWS.cs
@@ -0,0 +1,75 @@
+﻿#region License
+/*
+ * Koekiebox CONFIDENTIAL
+ *
+ * [2012] - [2017] Koekiebox (Pty) Ltd
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property
+ * of Koekiebox and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to Koekiebox
+ * and its suppliers and may be covered by South African and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Koekiebox.
+ */
+#endregion
+
+using FluidAPI.Com.Fluid.Vo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Base Fluid class for all value objects.
+/// </summary>
+/// <remarks>
+/// 
+/// </remarks>
+namespace FluidWSDotNetClient.Com.Fluid.Ws.Client.V1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class ABaseClientWS
+    {
+
+
+        public ABaseFluidJSONObject PostJSONWebRequest(ABaseFluidJSONObject objectToPostParam)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://url");
+
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = ABaseFluidJSONObject.ObjectToString(objectToPostParam);
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string responseJson = null;
+
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                responseJson = streamReader.ReadToEnd();
+            }
+
+            if(responseJson == null)
+            {
+                return null;
+            }
+
+            return (ABaseFluidJSONObject)ABaseFluidJSONObject.StringToObject(responseJson);
+        }
+
+    }
+}
